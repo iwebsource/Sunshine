921d15a650293613f2bb10f2bfc64259dc576e3d
iwebsource <iwebsource@eastlink.ca>
2014-07-17 17:16:43 -0300

    Stuck on 2.04 - Its driving me NUTS I tell YOU!!

--- app/src/main/java/com/test/astewart/sunshine/MainActivity.java
+++ app/src/main/java/com/test/astewart/sunshine/MainActivity.java
@@ -1,19 +1,18 @@
 package com.test.astewart.sunshine;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
-
-public class MainActivity extends Activity {
+public class MainActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         if (savedInstanceState == null) {
-            getFragmentManager().beginTransaction()
+            getSupportFragmentManager().beginTransaction()
                     .add(R.id.container, new ForecastFragment())
                     .commit();
         }
